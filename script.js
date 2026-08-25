6d5e1be4880d725e9974b5c687fc051dc6ca8d5f
Marichaljuan07 <Juanmarichal57@Gmail.com>
2026-08-25 18:30:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const MAX_QUESTIONS = 20;
 const MAX_GUESSES = 2;
 const FALLBACK_TIMEOUT_MS = 9000;
 const SUGGESTION_LIMIT = 5;
+const MIND_API_URL = "https://mind-duel-jxu8.onrender.com";
 
 const el = id => document.getElementById(id);
 const ui = {
